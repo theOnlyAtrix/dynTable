7d3c597125531d3f463ff723dfd03d181d8a4971
atrix... <strikez246@gmail.com>
2025-09-13 14:26:42 +0400

    Update periodicTableView.jsx

--- src/periodicTableView.jsx
+++ src/periodicTableView.jsx
@@ -1,9 +1,13 @@
 import "./main.css";
 import PeriodicTable from "./periodicTable.jsx";
+import { Helmet } from "react-helmet"
 
 function PeriodicTableView() {
     return (
         <>
+            <Helmet>
+                <title>DynTable: The Periodic Table Refined</title>
+            </Helmet>
             <div className="introCard">
                 <span className="animated-text"><h1 className="animated-text">Welcome </h1></span><span className="animated-text"><h1 className="animated-text">to </h1></span><span className="animated-text"><h1 className="animated-text">DynTable</h1></span>
                 <span className="animated-text"><h3>The Future of Elements starts here</h3></span>
