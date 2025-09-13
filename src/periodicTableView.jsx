57f4e0a499197337e91018e8943b2b03ff307e1d
atrix... <strikez246@gmail.com>
2025-09-13 18:01:28 +0400

    Update periodicTableView.jsx

--- src/periodicTableView.jsx
+++ src/periodicTableView.jsx
@@ -7,6 +7,8 @@ function PeriodicTableView() {
         <>
             <Helmet>
                 <title>DynTable: The Periodic Table Refined</title>
+                <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-4254925270009380"
+                 crossorigin="anonymous"></script>
             </Helmet>
             <div className="introCard">
                 <span className="animated-text"><h1 className="animated-text">Welcome </h1></span><span className="animated-text"><h1 className="animated-text">to </h1></span><span className="animated-text"><h1 className="animated-text">DynTable</h1></span>
