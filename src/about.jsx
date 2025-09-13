24971cebe94b5df4f94effa09666ae7d1d462290
atrix... <strikez246@gmail.com>
2025-09-13 18:07:24 +0400

    Update about.jsx

--- src/about.jsx
+++ src/about.jsx
@@ -7,8 +7,7 @@ function About() {
         <>
             <Helmet>
                 <title>About Dyntable</title>
-                <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-4254925270009380"
-                 crossorigin="anonymous"></script>
+                <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-4254925270009380" crossorigin="anonymous"></script>
             </Helmet>
             <div className="about">
                 <h1>About DynTable:</h1><br/><br/>
