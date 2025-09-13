6f4863288412578d3b9672cd007fbd087c1c6dfc
atrix... <strikez246@gmail.com>
2025-09-13 18:03:25 +0400

    Update about.jsx

--- src/about.jsx
+++ src/about.jsx
@@ -1,16 +1,24 @@
 import './propertyView.css';
 import {Link} from "react-router-dom";
+import { Helmet } from 'react-helmet';
 
 function About() {
     return (
-        <div className="about">
-            <h1>About DynTable:</h1><br/><br/>
-            <h3>
-                Dyntable is a reference website that is used by people to learn about the elements of the periodic table.<br/>
-                Please Note that this is used by people to learn about the elements of the periodic table. This is not for experimental-level use.<br/><br/>
-                This Website is Made by <Link to="https://github.com/theOnlyAtrix" style={{textAlign:"left"}} className="me">theOnlyAtrix</Link>.
-            </h3>
-        </div>
+        <>
+            <Helmet>
+                <title>About Dyntable</title>
+                <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-4254925270009380"
+                 crossorigin="anonymous"></script>
+            </Helmet>
+            <div className="about">
+                <h1>About DynTable:</h1><br/><br/>
+                <h3>
+                    Dyntable is a reference website that is used by people to learn about the elements of the periodic table.<br/>
+                    Please Note that this is used by people to learn about the elements of the periodic table. This is not for experimental-level use.<br/><br/>
+                    This Website is Made by <Link to="https://github.com/theOnlyAtrix" style={{textAlign:"left"}} className="me">theOnlyAtrix</Link>.
+                </h3>
+            </div>
+        </>
     );
 }
 
